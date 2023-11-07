3ee0752450440b5605ed9ac3011aa99784ae1f12
ngakoloic <bngako@outlook.fr>
2023-11-06 23:13:55 -0400

    Add swiper for section galeries

--- hairpro/src/components/Galeries.js
+++ hairpro/src/components/Galeries.js
@@ -1,10 +1,67 @@
 import React from 'react';
 
+// import Swiper core and required modules
+import { EffectCoverflow, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 const Galeries = () => {
     return (
-        <div>
-
-        </div>
+        <Swiper
+            style={{
+                '--swiper-pagination-color': '#fff',
+            }}
+            pagination={{
+                clickable: true,
+            }}
+            loop={true}
+            effect={'coverflow'}
+            slidesPerView={'auto'}
+            centeredSlides={true}
+            grabCursor={true}
+            coverflowEffect={{
+                rotate: 10,
+                stretch: 2,
+                depth: 80,
+                modifier: 2.5,
+                slideShadows: true,
+            }}
+            // install Swiper modules
+            modules={[EffectCoverflow, Pagination]}
+        // className='mySwiper'
+        >
+            <SwiperSlide>
+                <img src="img/coupe-homme.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src="img/coupe-homme.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src="img/coupe-homme.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src="img/coupe-homme.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src="img/coupe-homme.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src="img/coupe-homme.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src="img/coupe-homme.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src="img/coupe-homme.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src="img/coupe-homme.jpg" />
+            </SwiperSlide>
+        </Swiper>
     );
 };
 
